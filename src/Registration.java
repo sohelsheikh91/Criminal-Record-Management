7357b8eb2a8f45cdf4574de2985e40ed54a16f4c
Sohel Sheikh <sohelsheikh91@gmail.com>
2019-09-26 09:41:43 +0530

    8th commit

--- src/Registration.java
+++ src/Registration.java
@@ -1,7 +1,9 @@
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
-public class Registration {
+public class Registration extends NewJDBC  {
 	
 	public void signup() throws ClassNotFoundException, SQLException {
 		
@@ -22,5 +24,24 @@ public class Registration {
 		
 		System.out.println("Please job Profile :");
 		String Job = in.nextLine();
+		
+		
+		Statement st = conn.createStatement();
+		PreparedStatement mystr=null;
+		try {
+			String sql = "insert into registration  values(?,?,?,?,?,?,?,?,sysdate)";
+			
+			mystr = conn.prepareStatement(sql);
+			
+			mystr.setString(1,Name);
+			mystr.setString(2,user);
+			mystr.setString(3,Pass);
+			mystr.setString(4,Job);
+			
+		}
+		catch(Exception e){
+			
+			
+		}
 	}
 }
